a57e0fdab23a3f64e3ee799d2e4fe0ad0ea31c2d
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-08-24 13:41:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use("/", express.static(__dirname + "/public"));
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
 app.post('/validate-license', (req, res) => {
   const expiredKeys = ['B', 'A'];
   const licenseKey = req.body.licenseKey;
